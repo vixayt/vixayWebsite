99524ac9b9feb0ec56779688764c4c8acdb07fad
Ted Vixay <tvixay@tvixay.local>
2019-02-25 21:35:50 -0800

    removed pdf of resume. will make resume more tabular

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import pdf from '../static/vixayResume.pdf';
+import '@zendeskgarden/react-tabs/dist/styles.css'
+
+import { Tabs, TabPanel} from '@zendeskgarden/react-tabs'
+
+const tabs = ['Info', 'Experience', 'Education'];
 
 const Resume = () => (
   <div className="resume" >
-    <embed src={pdf} width="760px" height="1030px" />
+    <Tabs vertical>
+      <TabPanel label={tabs[0]} key={tabs[0]}>
+        <pre>
+          <code>
+            Hello, my name is Theodore Vixay.
+          </code>
+        </pre>
+      </TabPanel>
+      <TabPanel label={tabs[1]} key={tabs[1]}>
+        <div>
+          <div className="resume-company">
+            NIKE, Inc.
+          </div>
+            <p>
+              Position: SW QA Engineer
+            </p>
+        </div>
+      </TabPanel>
+      <TabPanel label={tabs[2]} key={tabs[2]}>
+        BL
+      </TabPanel>
+    </Tabs>
   </div>
-)
+);
 
 export default Resume;
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -4,6 +4,8 @@ import createHistory from 'history/createBrowserHistory';
 import HomePage from '../components/HomePage';
 import Header from '../components/Header';
 import Resume from '../components/Resume';
+import Stocks from '../components/Stocks';
+
 
 export const history = createHistory();
 
@@ -14,6 +16,7 @@ const AppRouter = () => (
       <Switch>
         <Route path="/" component={HomePage} exact={true} />
         <Route path="/resume" component={Resume} exact={true} />
+        <Route path="/stocks" component={Stocks} exact={true} />
       </Switch>
     </div>
   </Router>
